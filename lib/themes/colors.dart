d3bd64d40fc4b45f29410add6410acb50798c441
Cameron Scully <camscully@hotmail.com>
2021-03-24 17:29:35 +1100

    New color palettes

--- lib/themes/colors.dart
+++ lib/themes/colors.dart
@@ -1,10 +1,71 @@
 import 'package:flutter/material.dart';
 
-class CustomColors {
-  static const Color lightPurple = Color(0xFFBB86FA);
-  static const Color purple = Color(0xFF6002EE);
-  static const Color deepPurple = Color(0xFF3900B1);
-  static const Color grey = Color(0xFF848484);
-  static const Color darkGrey = Color(0xFF222222);
-  static const Color black = Color(0xFF141414);
+class Palette1 {
+  static const Color color1 = Color(0xFF222831);
+  static const Color color2 = Color(0xFF393E46);
+  static const Color color3 = Color(0xFF00ADB5);
+  static const Color color4 = Color(0xFFEEEEEE);
+}
+
+class Palette2 {
+  static const Color color1 = Color(0xFFF9ED69);
+  static const Color color2 = Color(0xFFF08A5D);
+  static const Color color3 = Color(0xFFB83B5E);
+  static const Color color4 = Color(0xFF6A2C70);
+}
+
+class Palette3 {
+  static const Color color1 = Color(0xFFF38181);
+  static const Color color2 = Color(0xFFFCE38A);
+  static const Color color3 = Color(0xFFEAFFD0);
+  static const Color color4 = Color(0xFF95E1D3);
+}
+
+class Palette4 {
+  static const Color color1 = Color(0xFF08D9D6);
+  static const Color color2 = Color(0xFF252A34);
+  static const Color color3 = Color(0xFFFF2E63);
+  static const Color color4 = Color(0xFFEAEAEA);
+}
+
+class Palette5 {
+  static const Color color1 = Color(0xFFE3FDFD);
+  static const Color color2 = Color(0xFFCBF1F5);
+  static const Color color3 = Color(0xFFA6E3E9);
+  static const Color color4 = Color(0xFF71C9CE);
+}
+
+class Palette6 {
+  static const Color color1 = Color(0xFFA8D8EA);
+  static const Color color2 = Color(0xFFAA96DA);
+  static const Color color3 = Color(0xFFFCBAD3);
+  static const Color color4 = Color(0xFFFFFFD2);
+}
+
+class Palette7 {
+  static const Color color1 = Color(0xFF364F6B);
+  static const Color color2 = Color(0xFF3FC1C9);
+  static const Color color3 = Color(0xFFF5F5F5);
+  static const Color color4 = Color(0xFFFC5185);
+}
+
+class Palette8 {
+  static const Color color1 = Color(0xFFFFC7C7);
+  static const Color color2 = Color(0xFFFFE2E2);
+  static const Color color3 = Color(0xFFF6F6F6);
+  static const Color color4 = Color(0xFF8785A2);
+}
+
+class Palette9 {
+  static const Color color1 = Color(0xFFF9F7F7);
+  static const Color color2 = Color(0xFFDBE2EF);
+  static const Color color3 = Color(0xFF3F72AF);
+  static const Color color4 = Color(0xFF112D4E);
+}
+
+class Palette10 {
+  static const Color color1 = Color(0xFFE4F9F5);
+  static const Color color2 = Color(0xFF30E3CA);
+  static const Color color3 = Color(0xFF11999E);
+  static const Color color4 = Color(0xFF40514E);
 }
